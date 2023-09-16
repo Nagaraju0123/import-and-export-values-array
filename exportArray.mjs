13ec1bba91cce40ec1e25628b070a3a0d4df158d
Nagaraju0123 <nimmalanagaraju1011@gmail.com>
2023-09-16 09:05:39 +0000

    solution

---
+++ exportArray.mjs
@@ -0,0 +1,6 @@
+const myArray = {
+  countries: 190,
+  continents: 7,
+  false: 6.2,
+};
+export default myArray;
---
+++ importArray.mjs
@@ -0,0 +1,4 @@
+import myArray from "./exportArray.mjs";
+console.log(myArray.countries);
+console.log(myArray.continents);
+console.log(myArray.false);
